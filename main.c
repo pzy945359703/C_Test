c0e101204450f37ff8da4f0a162e2e54dc19f17a
oogundam <945359703@qq.com>
2019-03-02 15:22:02 +0800

    new sum function

--- main.c
+++ main.c
@@ -2,10 +2,17 @@
 void pln(){
     printf("\nthis is the second func\n");
 }
-
+int sum(int a,int b){
+	int c;
+	c=a+b;
+	return c;
+}
 int main(){
     printf("this is the first seasion\n");
     //pln();
     //printf("this is the 3 seasion\n");
+    int x;
+    x=sum(1,4);
+    printf("%d",x);
     return 0;
 }

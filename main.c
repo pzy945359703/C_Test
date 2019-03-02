029d8f9c9a3bb5718020dbac58426815185e5c52
oogundam <945359703@qq.com>
2019-03-02 14:13:32 +0800

    上传测试

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+void pln(){
+    printf("\nthis is the second func\n");
+}
+
+int main(){
+    printf("this is the first seasion\n");
+    pln();
+    printf("this is the 3 seasion\n");
+    return 0;
+}

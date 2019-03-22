e4aafc2ccb3fb7d5d5258fecee579d164894be29
oogundam <945359703@qq.com>
2019-03-22 18:44:01 +0800

    提交信息

---
+++ SalesManagement/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesManagement.Models
+{
+    public class User
+    {
+        public virtual int UserId { get; set; }
+        public virtual string AccountNum { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string Username { get; set; }
+    }
+}
---
+++ SalesManagement/Models/UserDBInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesManagement.Models
+{
+    public class UserDBInitializer:System.Data.Entity.DropCreateDatabaseAlways<UserDBContext>
+    {
+        protected override void Seed(UserDBContext context)
+        {
+            context.Users.Add(new User { AccountNum = "pengzeyu@qq.com" ,Password="12345678",Username="彭泽宇1"});
+            base.Seed(context);
+        }
+    }
+}
